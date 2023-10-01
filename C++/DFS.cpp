6f078d75f316ef8ccae491915be0dedf383c9271
deepak74620 <105719092+deepak74620@users.noreply.github.com>
2023-10-01 10:33:23 +0530

    Create DFS.cpp
    Depth First Search technique to traverse in a graph

---
+++ C++/DFS.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  private: 
+    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
+        vis[node] = 1; 
+        ls.push_back(node); 
+        // traverse all its neighbours
+        for(auto it : adj[node]) {
+            // if the neighbour is not visited
+            if(!vis[it]) {
+                dfs(it, adj, vis, ls); 
+            }
+        }
+    }
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V] = {0}; 
+        int start = 0;
+        // create a list to store dfs
+        vector<int> ls; 
+        // call dfs for starting node
+        dfs(start, adj, vis, ls); 
+        return ls; 
+    }
+};
+
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector <int> &ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
+int main() 
+{
+    vector <int> adj[5];
+    
+    addEdge(adj, 0, 2);
+    addEdge(adj, 2, 4);
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 3);
+
+    Solution obj;
+    vector <int> ans = obj.dfsOfGraph(5, adj);
+    printAns(ans);
+
+    return 0;
+}
+
+// Output: 0 2 4 1 3 
+
+// Time Complexity: For an undirected graph, O(N) + O(2E), For a directed graph, O(N) + O(E), Because for every node we are calling the recursive function once, the time taken is O(N) and 2E is for total degrees as we traverse for all adjacent nodes.
+
+// Space Complexity: O(3N) ~ O(N), Space for dfs stack space, visited array and an adjacency list.
