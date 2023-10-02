6b03de0d52cc2846f70f3b02085e297e855222ae
Sadeesha_Jayaweea <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-10-02 11:27:14 +0530

    Implementation of Binary Search Algorithm

---
+++ C/BinarySearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//Binary Search Algorithm
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // If the target is found at the middle, return its index
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // If the target is greater, ignore the left half
+        if (arr[mid] < target) {
+            left = mid + 1;
+        }
+
+        // If the target is smaller, ignore the right half
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // Target not found in the array
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 12;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}
