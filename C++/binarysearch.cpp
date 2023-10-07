e78925577367d167b775966f8aebcd4be201f449
Ashutosh <113531606+ashutoshbins@users.noreply.github.com>
2023-10-07 19:23:15 +0530

    Update binarysearch.cpp
    took input in a vector and sorted it then asked user to enter the number he wants to search

--- C++/binarysearch.cpp
+++ C++/binarysearch.cpp
@@ -1,46 +1,57 @@
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+// Binary search function
+int binarySearch(const vector<int>& arr, int key) {
+    int left = 0;
+    int right = arr.size() - 1;
 
-int binarySearch(int arr[], int l, int r, int x)
-{
-	while (l <= r) {
-		int m = l + (r - l) / 2;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
-		if (arr[m] == x)
-			return m;
+        // If the key is found at the middle index, return its position
+        if (arr[mid] == key) {
+            return mid;
+        }
 
-		if (arr[m] < x)
-			l = m + 1;
-
-		else
-			r = m - 1;
-	}
-
-
-	return -1;
-}
-
-
-int main()
-{
-    int x;
-    cout<<"Enter size of array ";
-	cin>>x;
-    int arr[x];
-    
-    for (int i = 0; i < x; i++)
-    {
-        cin >> arr[i];
+        // If the key is smaller than the middle element, search the left half
+        if (arr[mid] > key) {
+            right = mid - 1;
+        }
+        // If the key is larger than the middle element, search the right half
+        else {
+            left = mid + 1;
+        }
     }
 
-	int key;
-    cout<<"enter key ";
-    cin>>key;
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int result = binarySearch(arr, 0, n - 1, key);
-	(result == -1)
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result;
-	return 0;
+    // Key not found in the array
+    return -1;
 }
+
+int main() {
+    int n;
+    vector<int> arr;
+	cout<<"Enter the size ";
+	cin>>n;
+	int k;
+	for(int i=0;i<n;i++)
+	{
+		cin>>k;
+	arr.push_back(k);
+	}
+    cout<<"Enter the element you want to search ";
+	cin>>k;
+	sort(arr.begin(), arr.end());
+
+    int result = binarySearch(arr, k);
+
+    if (result != -1) {
+        cout << "Element " << k << " found at index after sorting the array in ascending order " << result << endl;
+    } else {
+        cout << "Element " << k << " not found in the array" << endl;
+    }
+
+    return 0;
+}
+
