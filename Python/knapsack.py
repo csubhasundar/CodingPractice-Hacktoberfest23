1464c94bad5a83406ff0745171fe296745304276
Saptadeep Banerjee <saptadeep555@gmail.com>
2023-10-08 17:46:58 +0530

    added knapsack

---
+++ Python/knapsack.py
@@ -0,0 +1,10 @@
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            if weights[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][capacity]
